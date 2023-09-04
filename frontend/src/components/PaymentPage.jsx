b992f9b46cc1937ff6c2a67767b853c1bf176155
parth-5097 <parth5097@gmail.com>
2023-09-03 21:36:53 -0400

    Add comments for better understanding of frontend

--- frontend/src/components/PaymentPage.jsx
+++ frontend/src/components/PaymentPage.jsx
@@ -6,49 +6,47 @@ import { API_URL } from '../config/index';
 import './PaymentPage.css';
 
 const PaymentPage = () => {
-    console.log("cjdbkcdkj");
-	const location = useLocation();
+    // Get the current location using react-router's useLocation hook
+    const location = useLocation();
 
-	useEffect(() => {
-		// Check to see if this is a redirect back from Checkout
-		// const query = new URLSearchParams(window.location.search);
-		const values = QueryString.parse(location.search);
+    useEffect(() => {
+        // Parse query parameters from the URL
+        const values = QueryString.parse(location.search);
 
-		if (values.success) {
-			console.log(
-				'Order placed! You will receive an email confirmation.'
-			);
-		}
+        // Check if the payment was successful
+        if (values.success) {
+            console.log('Order placed! You will receive an email confirmation.');
+        }
 
-		if (values.canceled) {
-			console.log(
-				"Order canceled -- continue to shop around and checkout when you're ready."
-			);
-		}
-	}, []);
+        // Check if the payment was canceled
+        if (values.canceled) {
+            console.log("Order canceled -- continue to shop around and checkout when you're ready.");
+        }
+    }, []); // The empty dependency array ensures this effect runs once on component mount
 
-	return (
-		<section>
-			<div className='product'>
-				<img
-					src='https://imgur.com/EJOjIMC.png'
-					alt='The cover of Stripe demo'
-				/>
-				<div className='description'>
-					<h5>$20.00</h5>
-					<h3>Make Payment</h3>
-				</div>
-			</div>
-			<form
-				action={`${API_URL}/stripe/create-checkout-session`}
-				method='POST'
-			>
-				<button className='button' type='submit'>
-					Checkout
-				</button>
-			</form>
-		</section>
-	);
+    return (
+        // Render the PaymentPage component's content
+        <section>
+            <div className='product'>
+                <img
+                    src='https://imgur.com/EJOjIMC.png'
+                    alt='The cover of Stripe demo'
+                />
+                <div className='description'>
+                    <h5>$20.00</h5>
+                    <h3>Make Payment</h3>
+                </div>
+            </div>
+            <form
+                action={`${API_URL}/stripe/create-checkout-session`}
+                method='POST'
+            >
+                <button className='button' type='submit'>
+                    Checkout
+                </button>
+            </form>
+        </section>
+    );
 };
 
 export default PaymentPage;
